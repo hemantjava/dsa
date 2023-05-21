22bcd3df5af8e91fb7f0d6ca0d546a4f3fa28fa1
Hemant <Hemantjava90@gmail.com>
2023-05-21 19:37:14 +0530

    21/05/2023

--- src/main/java/com/hemant/other/string/EvaluateExpression.java
+++ src/main/java/com/hemant/other/string/EvaluateExpression.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class EvaluateExpression {
     public static void main(String[] args) {
         String expression = "10x20/2x5/4x2";
-        int result = evaluateExpression(expression.replace("x", "*"));
+        int result = evaluateExpression(expression);
         System.out.println(expression + " = " + result);
     }
 
@@ -13,13 +13,13 @@ public class EvaluateExpression {
         String[] terms = expression.split("/");
 
         return Arrays.stream(terms)
-                .map(EvaluateExpression::evaluateTerm)
+                .map(EvaluateExpression::evaluateTerm)//200,10,8
                 .reduce((a, b) -> a / b)
                 .orElse(0);
     }
 
     public static int evaluateTerm(String term) {
-        String[] factors = term.split("\\*");
+        String[] factors = term.split("x");
 
         return Arrays.stream(factors)
                 .mapToInt(Integer::parseInt)
