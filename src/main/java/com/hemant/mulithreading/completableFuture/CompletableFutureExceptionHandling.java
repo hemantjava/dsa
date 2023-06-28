6a901bbd43ecaecde5f830801df52b3acf1913f4
Hemant <Hemantjava90@gmail.com>
2023-06-28 10:31:22 +0530

    28/06/2023-1

---
+++ src/main/java/com/hemant/mulithreading/completableFuture/CompletableFutureExceptionHandling.java
@@ -0,0 +1,29 @@
+package com.hemant.mulithreading.completableFuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureExceptionHandling {
+
+    public static void main(String[] args) {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            // Simulating a task that may throw an exception
+            if (Math.random() < 0.5) { //if (true)
+                throw new RuntimeException("Task failed!");
+            }
+            return "Task completed successfully";
+        });
+
+        completableFuture.exceptionally(ex -> {
+            System.out.println("Exception occurred: " + ex.getMessage());
+            return "Fallback value"; // provide a fallback value in case of exception
+        });
+
+        try {
+            String result = completableFuture.get(); // blocking call to get the result
+            System.out.println("Result: " + result);
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println("Exception occurred while executing the task: " + e.getMessage());
+        }
+    }
+}
